90c3baf3bdd9d41a6700d92359493f348cc77716
ErickFoxes <00092217@uca.edu.sv>
2019-05-03 15:14:29 -0600

    Mi lastimoso intento de ver que se puede hacer

---
+++ app/src/main/java/com/example/evaluado/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.evaluado
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+/* Lo que hay aquí no esta en un fragmento XD, hice los fragmentos, pero no los puse
+No se si es malo, poner ttodo esto
+En el siguente me recupero... espero...
+ no se que como se hace :T Lo siento (a yo mismo)
+ PD: me da pena ser el primero en salir
+
+  (  )_____(  )
+ |  _________  |
+ | (         ) |
+ | ( O  U  O ) |
+ | (________') |
+ |_____________|_
+ ( )         ( ) |
+ ( )         ( ) |
+
+
+ */
+
+//class MainActivity : AppCompatActivity(), nextbackFragment.OnFragmentInteractionListener, ColorcitoequisdeFragment.OnFragmentInteractionListener{
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    //Aquí debería ir mi código
+    //Si supiera como hacerlo!!! O-o*
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
